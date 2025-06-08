9b82146be4df98c6b192749963c0192e6408eb14
Utkarsh <utkarshshrivastava.13@gmail.com>
2025-06-08 17:12:11 +0530

    minor-update-02

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { User } from 'lucide-react';
-import Logo from '../assets/csitlogo.png';
-import Image1 from '../assets/Aicte-logo.png';
-import Image2 from '../assets/Institutions innovation council.png';
-import Image3 from '../assets/ISO_logo.png';
-import Image4 from '../assets/CSVTU.png';
-import Image5 from '../assets/dataquest.png';
+import React, { useState } from "react";
+import { User } from "lucide-react";
+import Logo from "../assets/csitlogo.png";
+import Image1 from "../assets/Aicte-logo.png";
+import Image2 from "../assets/Institutions innovation council.png";
+import Image3 from "../assets/ISO_logo.png";
+import Image4 from "../assets/CSVTU.png";
+import Image5 from "../assets/dataquest.png";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,34 +31,70 @@ const Header = () => {
         <div className="hidden md:flex justify-between items-center container mx-auto">
           {/* Logo and Title */}
           <div className="flex items-center space-x-6">
-            <img src={Logo} alt="CSIT Logo" className="w-40 h-auto rounded-full" />
+            <img
+              src={Logo}
+              alt="CSIT Logo"
+              className="w-40 h-auto rounded-full"
+            />
             <div>
-              <h1 className="text-3xl text-black font-serif">Chhatrapati Shivaji Group of Institutions</h1>
-              <p className="text-sm text-black">Shivaji Nagar, Baload Road Durg, Chhattisgarh 491001</p>
+              <h1 className="text-3xl text-black font-serif">
+                Chhatrapati Shivaji Group of Institutions
+              </h1>
+              <p className="text-sm text-black">
+                Shivaji Nagar, Baload Road Durg, Chhattisgarh 491001
+              </p>
             </div>
           </div>
-          
+
           {/* Additional Photos - Desktop Only */}
           <div className="flex space-x-3">
-            <img src={Image1} alt="Sample 1" className="h-12 w-auto rounded-md" />
-            <img src={Image2} alt="Sample 2" className="h-12 w-auto rounded-md" />
-            <img src={Image3} alt="Sample 3" className="h-12 w-auto rounded-md" />
-            <img src={Image4} alt="Sample 4" className="h-12 w-auto rounded-md" />
-            <img src={Image5} alt="Sample 5" className="h-12 w-auto rounded-md" />
+            <img
+              src={Image1}
+              alt="Sample 1"
+              className="h-12 w-auto rounded-md"
+            />
+            <img
+              src={Image2}
+              alt="Sample 2"
+              className="h-12 w-auto rounded-md"
+            />
+            <img
+              src={Image3}
+              alt="Sample 3"
+              className="h-12 w-auto rounded-md"
+            />
+            <img
+              src={Image4}
+              alt="Sample 4"
+              className="h-12 w-auto rounded-md"
+            />
+            <img
+              src={Image5}
+              alt="Sample 5"
+              className="h-12 w-auto rounded-md"
+            />
           </div>
         </div>
-        
+
         {/* Mobile View - Left Logo, Right Info */}
         <div className="md:hidden flex justify-between items-center">
           {/* Logo on Left - Increased Size */}
           <div className="flex-shrink-0">
-            <img src={Logo} alt="CSIT Logo" className="w-24 h-auto rounded-full" />
+            <img
+              src={Logo}
+              alt="CSIT Logo"
+              className="w-24 h-auto rounded-full"
+            />
           </div>
-          
+
           {/* Institution Info on Right */}
           <div className="text-right flex-grow ml-3">
-            <h1 className="text-sm text-black font-serif">Chhatrapati Shivaji Institute of Technology</h1>
-            <p className="text-xs text-black">Shivaji Nagar, Baload Road Durg, Chhattisgarh 491001</p>
+            <h1 className="text-sm text-black font-serif">
+              Chhatrapati Shivaji Group of Institutions
+            </h1>
+            <p className="text-xs text-black">
+              Shivaji Nagar, Baload Road Durg, Chhattisgarh 491001
+            </p>
           </div>
         </div>
       </div>
